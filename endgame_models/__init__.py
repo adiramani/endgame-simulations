debfd9c31542d1c6148b2c34fd66bb76914748e9
mark-todd <markpeter.todd@hotmail.co.uk>
2023-01-13 12:13:50 +0000

    Added first version of endgame models

---
+++ endgame_models/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    EndgameModel, 
+    convert_pydantic, 
+    reduce_parameters, 
+    BaseInitialParams, 
+    BaseProgramParams
+)
+
+__all__ = [
+    'EndgameModel', 
+    'convert_pydantic', 
+    'reduce_parameters', 
+    'BaseInitialParams',
+    'BaseProgramParams'
+]
---
+++ examples/hedgedoc_test.py
@@ -0,0 +1,88 @@
+
+from endgame_models import BaseInitialParams, BaseProgramParams, EndgameModel, convert_pydantic, reduce_parameters
+from enum import Enum
+
+test = {
+    "parameters": {
+        "initial": {
+            "w_rate": 0.1,
+            "delta_time": 3
+        },
+        "changes": [
+            {
+                "year": 2020,
+                "month": 1,
+                "params": {
+                    "delta_time": 1
+                }
+            },
+            {
+                "year": 2022,
+                "month": 1, 
+                "params": {
+                    "delta_time": 2,
+                    "test": 1
+                }
+            }
+        ]
+    },
+    "programs": [
+        {
+            "first_year": 2020,
+            "first_month": 1, 
+            "last_year": 2022,
+            "last_month": 12, 
+            "interventions": {
+                "min_age": 4,
+                "max_age": 5,
+                "coverage": 0.7,
+                "treatment_interval": 0.1,
+                "drug_efficacy": 0.85
+            }
+        },
+        {
+            "first_year": 2026,
+            "last_year": 2029,
+            "drug_efficacy": 0.50,
+            "interventions": [
+                {
+                    "min_age": 2,
+                    "max_age": 5,
+                    "coverage": 0.8,
+                    "treatment_interval": 0.05,
+                    "type": "vaccine"
+                },
+                {
+                    "min_age": 2,
+                    "max_age": 5,
+                    "coverage": 0.8,
+                    "treatment_interval": 0.05,
+                    "type": "MDA"
+                }
+            ]
+        }
+    ]
+}
+
+class TestParams(BaseInitialParams):
+    w_rate: int
+    delta_time: int
+
+
+class VaccineType(Enum):
+    MDA = "MDA"
+    vaccine = "vaccine"
+
+class TestProgramParams(BaseProgramParams):
+    min_age: int
+    max_age: int
+    coverage: float
+    type: VaccineType = VaccineType.MDA
+
+NewModel = EndgameModel[TestParams, convert_pydantic(TestParams), TestProgramParams]
+output = NewModel.parse_obj(test)
+print(output.parameters)
+
+
+reduced = reduce_parameters(output.parameters.initial, output.parameters.changes)
+print(reduced)
