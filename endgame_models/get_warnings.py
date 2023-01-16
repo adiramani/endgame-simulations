b121ea1de48d4136e01a650fe993f66504a8bb55
mark-todd <markpeter.todd@hotmail.co.uk>
2023-01-16 13:01:04 +0000

    Added initial rework

--- endgame_models/get_warnings.py
+++ endgame_models/get_warnings.py
@@ -1,46 +1,41 @@
-from dataclasses import dataclass
-from difflib import get_close_matches
-from typing import Dict, Union
-
 __all__ = ["get_warnings"]
 
+from dataclasses import dataclass
+from difflib import get_close_matches
+
+
 Flat = str | float | int | bool
-JSONType = Union[
-    Flat,
-    list["JSONType"],
-    dict[str, "JSONType"],
-    tuple["JSONType", ...],
-]
+
+JSONType = Flat | list["JSONType"] | dict[str, "JSONType"] | tuple["JSONType", ...]
 DictType = dict[str, JSONType]
 ListType = list[JSONType]
 TupleType = tuple[JSONType, ...]
 
-
 @dataclass
 class DifferenceData:
-    value: JSONType
     matches: list[str]
 
+_MatchType = DifferenceData | list["_MatchType"] | dict[str, "_MatchType"]
+ListMatchType = list["_MatchType"]
+DictMatchType = dict[str, "_MatchType"]
 
-FlattenedDict = Dict[str, Flat | DifferenceData]
+FlattenedDict = dict[str, Flat | DifferenceData]
 
 
-def compare_dicts(data_dict: DictType, model_dict: DictType):
-    def compare_iterable(list1: ListType | TupleType, list2: ListType | TupleType):
+def compare_dicts(data_dict: DictType, model_dict: DictType) -> DictMatchType:
+    def compare_iterable(list1: ListType | TupleType, list2: ListType | TupleType) -> ListMatchType:
         output_list = []
-        for i, item in enumerate(list1):
-            other_item = list2[i]
+        for item, other_item in zip(list1, list2):
             if isinstance(item, dict):
                 assert isinstance(other_item, dict)
                 out = compare_dicts(item, other_item)
-            elif isinstance(item, list):
-                assert isinstance(other_item, list)
+            elif isinstance(item, (list, tuple)):
+                assert isinstance(other_item, type(item))
                 out = compare_iterable(item, other_item)
             else:
-                assert isinstance(item, tuple)
-                assert isinstance(other_item, tuple)
-                out = compare_iterable(item, other_item)
-            output_list += [out]
+                out = None
+            if out is not None:
+                output_list += [out]
         return output_list
 
     difference_dict = {}
@@ -51,30 +46,25 @@ def compare_dicts(data_dict: DictType, model_dict: DictType):
                 if isinstance(v, dict):
                     assert isinstance(other_v, dict)
                     difference_dict[k] = compare_dicts(v, other_v)
-                elif isinstance(v, list):
-                    assert isinstance(other_v, list)
+                elif isinstance(v, (list, tuple)):
+                    assert isinstance(other_v, type(v))
                     difference_dict[k] = compare_iterable(v, other_v)
-                elif isinstance(v, tuple):
-                    assert isinstance(v, tuple)
-                    assert isinstance(other_v, tuple)
-                    difference_dict[k] = compare_iterable(v, other_v)
-            else:
-                pass
+
         else:
             m = get_close_matches(k, model_dict.keys())
             if len(m) != 0:
-                difference_dict[k] = DifferenceData(v, m)
+                difference_dict[k] = DifferenceData(m)
     return difference_dict
 
 
-def outer_flatten(obj: DictType | ListType | TupleType, prefix: str = "") -> list[str]:
+def outer_flatten(obj: DictMatchType, prefix: str = "") -> list[str]:
     def flatten(
-        obj: DictType | ListType | TupleType, prefix: str = ""
+        obj: DictMatchType | ListMatchType, prefix: str = ""
     ) -> FlattenedDict:
         iterator = obj.items() if isinstance(obj, dict) else enumerate(obj)
         ret = {}
         for k, v in iterator:
-            if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
+            if isinstance(v, dict) or isinstance(v, list):
                 update = flatten(v, prefix=f"{prefix} -> {k}")
             else:
                 update = {f"Close match detected for: \n{prefix} -> {k}": v}
@@ -90,5 +80,5 @@ def outer_flatten(obj: DictType | ListType | TupleType, prefix: str = "") -> lis
     ]
 
 
-def get_warnings(data_dict: DictType, model_dict: DictType, prefix: str):
+def get_warnings(data_dict: DictType, model_dict: DictType, prefix: str) -> list[str]:
     return outer_flatten(compare_dicts(data_dict, model_dict), prefix=prefix)
