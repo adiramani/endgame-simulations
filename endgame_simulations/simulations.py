d0338e78f2463d1dd53dd3cfce2f380e5a250ba9
Marcin Krupowicz <ma@rcin.me>
2023-01-19 10:45:02 +0000

    Fixing a bug when time was advanced before the state

--- endgame_simulations/simulations.py
+++ endgame_simulations/simulations.py
@@ -221,8 +221,8 @@ class GenericSimulation(Generic[ParamsModel, State], ABC):
                         sampling_years_idx += 1
 
                 progress_bar.update(self._delta_time)
-                self.state.current_time += self._delta_time
                 type(self).advance_state(self.state, self.debug)
+                self.state.current_time += self._delta_time
 
     def run(self, *, end_time: float) -> None:
         """Run simulation from current state till `end_time`
@@ -240,5 +240,5 @@ class GenericSimulation(Generic[ParamsModel, State], ABC):
         ) as progress_bar:
             while self.state.current_time <= end_time:
                 progress_bar.update(self._delta_time)
-                self.state.current_time += self._delta_time
                 type(self).advance_state(self.state)
+                self.state.current_time += self._delta_time
